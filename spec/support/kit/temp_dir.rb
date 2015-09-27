de2ea7e4b39f16b97a7cab32cbd035441382ef9a
Brooke Kuhlmann <brooke@alchemists.io>
2015-09-27 08:21:17 -0600

    Updated RSpec support kit with new Gemsmith changes.

--- spec/support/kit/temp_dir.rb
+++ spec/support/kit/temp_dir.rb
@@ -2,6 +2,7 @@ require "rspec/core/shared_context"
 
 module RSpec
   module Kit
+    # Adds temp directory support to specs enabled with the :temp_dir metadata key.
     module TempDirContext
       extend RSpec::Core::SharedContext
       let(:temp_dir) { File.expand_path "../../../../tmp/rspec", __FILE__ }
@@ -13,9 +14,10 @@ RSpec.configure do |config|
   config.include RSpec::Kit::TempDirContext
 
   config.before do |example|
-    if example.metadata[:temp_dir]
-      FileUtils.rm_rf(temp_dir) if File.exist?(temp_dir)
-      FileUtils.mkdir_p(temp_dir)
-    end
+    FileUtils.mkdir_p(temp_dir) if example.metadata[:temp_dir]
+  end
+
+  config.after do |example|
+    FileUtils.rm_rf(temp_dir) if example.metadata[:temp_dir]
   end
 end
