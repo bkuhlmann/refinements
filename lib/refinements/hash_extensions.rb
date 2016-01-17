7cc6bc79ae8a2a100ea650c07e998cd84844efa8
Brooke Kuhlmann <brooke@alchemists.io>
2016-01-16 19:52:46 -0700

    Added Hash refinements.
    - Includes the following methods:
      - `#reverse_merge`
      - `#reverse_merge!`
      - `#deep_merge`
      - `#deep_merge!`

---
+++ lib/refinements/hash_extensions.rb
@@ -0,0 +1,32 @@
+module Refinements
+  # Refinements for Hashes.
+  module HashExtensions
+    refine Hash do
+      def deep_merge other_hash
+        dup.deep_merge! other_hash
+      end
+
+      def deep_merge! other_hash
+        other_hash.each.with_object self do |(other_key, other_value), original_hash|
+          current_value = original_hash[other_key]
+          original_hash[other_key] = deep_merge_value current_value, other_value
+        end
+      end
+
+      def reverse_merge other_hash
+        other_hash.merge self
+      end
+
+      def reverse_merge! other_hash
+        merge!(other_hash) { |_, left_value, _| left_value }
+      end
+
+      private
+
+      def deep_merge_value current_value, other_value
+        return current_value.deep_merge(other_value) if current_value.is_a?(Hash) && other_value.is_a?(Hash)
+        other_value
+      end
+    end
+  end
+end
---
+++ spec/lib/refinements/hash_extensions_spec.rb
@@ -0,0 +1,82 @@
+require "spec_helper"
+require "refinements/hash_extensions"
+
+RSpec.describe Refinements::HashExtensions do
+  using Refinements::HashExtensions
+
+  subject do
+    {
+      label: "Kaleidoscope",
+      categories: {
+        colors: %w(red black white cyan),
+        styles: %w(solid dotted)
+      },
+      tags: {
+        emoji: {
+          faces: {
+            happy: %w(happy estatic),
+            confused: %w(pensive sweating),
+            sad: "frown"
+          }
+        },
+        positive: "red",
+        negative: "black"
+      }
+    }
+  end
+
+  describe "#deep_merge" do
+    it "merges nested hash" do
+      proof = subject.merge tags: {emoji: {faces: "test"}, positive: "red", negative: "black", neutral: "grey"}
+      result = subject.deep_merge tags: {emoji: {faces: "test"}, neutral: "grey"}
+
+      expect(result).to eq(proof)
+    end
+
+    it "does not modify itself" do
+      proof = subject.dup
+      subject.deep_merge categories: {colors: %w(yellow brown)}
+
+      expect(subject).to eq(proof)
+    end
+  end
+
+  describe "#deep_merge!" do
+    it "modifies itself" do
+      proof = subject.merge tags: {emoji: {faces: "test"}, positive: "red", negative: "black"}
+      subject.deep_merge! tags: {emoji: {faces: "test"}}
+
+      expect(subject).to eq(proof)
+    end
+  end
+
+  describe "#reverse_merge" do
+    it "answers itself when keys match" do
+      result = subject.reverse_merge label: "empty", categories: "empty", tags: "empty"
+      expect(result).to eq(subject)
+    end
+
+    it "answers merged keys not part of self" do
+      proof = subject.dup.merge test: "example"
+      result = subject.reverse_merge test: "example"
+
+      expect(result).to eq(proof)
+    end
+
+    it "does not modify itself" do
+      proof = subject.dup
+      subject.reverse_merge test: "Example"
+
+      expect(subject).to eq(proof)
+    end
+  end
+
+  describe "#reverse_merge!" do
+    it "modifies itself" do
+      proof = subject.dup.merge test: "example"
+      subject.reverse_merge! test: "example"
+
+      expect(subject).to eq(proof)
+    end
+  end
+end
