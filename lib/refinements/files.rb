e4412899c2069164487bc80ec0c2d9d911b9ecff
Brooke Kuhlmann <brooke@alchemists.io>
2020-06-07 07:07:51 -0600

    Added File rewrite deprecation warning
    The original design, for rewritting files, was to support files and
    pathnames. In practice, `Pathname` objects ended up being the better
    choice. By removing support from files, this'll encourage more use of
    pathnames which lead to better design anyway.
    
    Support for `File.rewrite` will be dropped completely in the next major
    version.

--- lib/refinements/files.rb
+++ lib/refinements/files.rb
@@ -4,6 +4,7 @@ module Refinements
   module Files
     refine File.singleton_class do
       def rewrite path
+        warn "[DEPRECATION]: File.rewrite is deprecated, use Pathname#rewrite instead."
         read(path).then { |content| write path, yield(content) }
       end
     end
--- spec/lib/refinements/files_spec.rb
+++ spec/lib/refinements/files_spec.rb
@@ -14,5 +14,10 @@ RSpec.describe Refinements::Files do
       File.rewrite(test_path) { |content| "#{content} Two" }
       expect(File.read(test_path)).to eq("One Two")
     end
+
+    it "outputs deprecation warning" do
+      result = proc { File.rewrite(test_path) { |content| "#{content} Two" } }
+      expect(&result).to output(/DEPRECATION/).to_stderr
+    end
   end
 end
