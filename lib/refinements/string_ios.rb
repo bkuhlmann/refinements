3f6a3fb284047cc60dca37932fab55eada59628b
Brooke Kuhlmann <brooke@alchemists.io>
2020-07-30 19:36:42 -0600

    Added String IO rewind
    Handy when wanting to write to a string IO instance once, or even
    multiple times, and easily read everything back with a single method.

---
+++ lib/refinements/string_ios.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "stringio"
+
+module Refinements
+  module StringIOs
+    refine StringIO do
+      def reread length = nil, buffer: nil
+        tap(&:rewind).read length, buffer
+      end
+    end
+  end
+end
---
+++ spec/lib/refinements/string_ios_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Refinements::StringIOs do
+  using described_class
+
+  subject(:io) { StringIO.new }
+
+  describe "#rewind" do
+    before { io.write "This is a test." }
+
+    it "answers full string" do
+      expect(io.reread).to eq("This is a test.")
+    end
+
+    it "answers partial string" do
+      expect(io.reread(4)).to eq("This")
+    end
+
+    it "writes to buffer" do
+      buffer = +""
+      io.reread buffer: buffer
+
+      expect(buffer).to eq("This is a test.")
+    end
+  end
+end
