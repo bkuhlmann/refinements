6103d54e76aadbf70cdde4ff78709c2d678d67a5
Brooke Kuhlmann <brooke@alchemists.io>
2020-12-29 15:29:30 -0700

    Added version release notes

--- lib/refinements/identity.rb
+++ lib/refinements/identity.rb
@@ -5,7 +5,7 @@ module Refinements
   module Identity
     NAME = "refinements"
     LABEL = "Refinements"
-    VERSION = "8.0.0"
+    VERSION = "8.0.1"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
   end
 end
