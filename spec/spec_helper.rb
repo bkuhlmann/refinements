a99a9c0171baff4637a3cc6c6efa3c0f3ca5cd4c
Brooke Kuhlmann <brooke@alchemists.io>
2021-10-02 11:27:47 -0600

    Removed RSpec spec helper GC automatic compaction
    While GC compaction hasn't had a huge effect on small projects such as
    this, it can cause specs to run longer by several minutes on larger
    projects. Additionally, with new Ruby
    [Debug](https://github.com/ruby/debug) support, GC compaction can
    occasionally cause stack dumps. Due to all of these concerns, I am
    removing support until performance and reliability improves.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,9 +8,6 @@ SimpleCov.start { enable_coverage :branch }
 
 require "refinements"
 
-GC.auto_compact = true
-GC.verify_compaction_references double_heap: true, toward: :empty
-
 using Refinements::Pathnames
 
 Pathname.require_tree __dir__, "support/shared_contexts/**/*.rb"
