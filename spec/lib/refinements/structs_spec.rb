4dd2d2e2b8de3d12fa48f6d8988ce608a14c7eea
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-05 08:38:45 -0700

    Refactored struct specs to use anonymous structs
    A minor bit of clean up and reduction in object setup.

--- spec/lib/refinements/structs_spec.rb
+++ spec/lib/refinements/structs_spec.rb
@@ -7,104 +7,102 @@ RSpec.describe Refinements::Structs do
 
   describe "#keyworded?" do
     it "answers true when constructed with keyword arguments" do
-      blueprint = Struct.new :a, keyword_init: true
-      expect(blueprint.keyworded?).to eq(true)
+      struct = Struct.new :a, keyword_init: true
+      expect(struct.keyworded?).to eq(true)
     end
 
     it "answers false when constructed with positional arguments" do
-      blueprint = Struct.new :a
-      expect(blueprint.keyworded?).to eq(false)
+      struct = Struct.new :a
+      expect(struct.keyworded?).to eq(false)
     end
   end
 
   describe ".with_keywords" do
     context "with positional construction" do
-      let(:blueprint) { Struct.new :a, :b, :c }
+      let(:struct) { Struct.new :a, :b, :c }
 
       it "answers struct with all positions filled" do
-        expect(blueprint.with_keywords(a: 1, b: 2, c: 3)).to eq(blueprint[1, 2, 3])
+        expect(struct.with_keywords(a: 1, b: 2, c: 3)).to eq(struct[1, 2, 3])
       end
 
       it "answers struct with first position filled" do
-        expect(blueprint.with_keywords(a: 1)).to eq(blueprint[1, nil, nil])
+        expect(struct.with_keywords(a: 1)).to eq(struct[1, nil, nil])
       end
 
       it "answers struct with last position filled" do
-        expect(blueprint.with_keywords(c: 1)).to eq(blueprint[nil, nil, 1])
+        expect(struct.with_keywords(c: 1)).to eq(struct[nil, nil, 1])
       end
     end
 
     context "with keyword construction" do
-      let(:blueprint) { Struct.new :a, :b, :c, keyword_init: true }
+      let(:struct) { Struct.new :a, :b, :c, keyword_init: true }
 
       it "answers struct with all positions filled" do
-        expect(blueprint.with_keywords(a: 1, b: 2, c: 3)).to eq(blueprint[a: 1, b: 2, c: 3])
+        expect(struct.with_keywords(a: 1, b: 2, c: 3)).to eq(struct[a: 1, b: 2, c: 3])
       end
 
       it "answers struct with some positions filled" do
-        expect(blueprint.with_keywords(a: 1)).to eq(blueprint[a: 1, b: nil, c: nil])
+        expect(struct.with_keywords(a: 1)).to eq(struct[a: 1, b: nil, c: nil])
       end
     end
   end
 
   describe ".with_positions" do
     context "with positional construction" do
-      let(:blueprint) { Struct.new :a, :b, :c }
+      let(:struct) { Struct.new :a, :b, :c }
 
       it "answers struct with all positions filled" do
-        expect(blueprint.with_positions(1, 2, 3)).to eq(blueprint[1, 2, 3])
+        expect(struct.with_positions(1, 2, 3)).to eq(struct[1, 2, 3])
       end
 
       it "answers struct with some positions filled" do
-        expect(blueprint.with_positions(1)).to eq(blueprint[1, nil, nil])
+        expect(struct.with_positions(1)).to eq(struct[1, nil, nil])
       end
     end
 
     context "with keyword construction" do
-      let(:blueprint) { Struct.new :a, :b, :c, keyword_init: true }
+      let(:struct) { Struct.new :a, :b, :c, keyword_init: true }
 
       it "answers struct with all positions filled" do
-        expect(blueprint.with_positions(1, 2, 3)).to eq(blueprint[a: 1, b: 2, c: 3])
+        expect(struct.with_positions(1, 2, 3)).to eq(struct[a: 1, b: 2, c: 3])
       end
 
       it "answers struct with some positions filled" do
-        expect(blueprint.with_positions(1)).to eq(blueprint[a: 1, b: nil, c: nil])
+        expect(struct.with_positions(1)).to eq(struct[a: 1, b: nil, c: nil])
       end
     end
   end
 
   shared_examples_for "a merge" do |method|
     context "with positional construction" do
-      let(:blueprint) { Struct.new :a, :b, :c }
-      let(:struct) { blueprint[1, 2, 3] }
+      let(:struct) { Struct.new(:a, :b, :c).new 1, 2, 3 }
 
-      it "answers struct with all attributes rebuilt" do
-        expect(struct.public_send(method, a: 7, b: 8, c: 9)).to eq(blueprint[7, 8, 9])
+      it "answers struct with attributes rebuilt" do
+        expect(struct.public_send(method, a: 7, b: 8, c: 9)).to eq(struct.class[7, 8, 9])
       end
 
       it "answers struct with some attributes rebuilt" do
-        expect(struct.public_send(method, a: 7, c: 9)).to eq(blueprint[7, 2, 9])
+        expect(struct.public_send(method, a: 7, c: 9)).to eq(struct.class[7, 2, 9])
       end
 
       it "answers identical struct with no arguments" do
-        expect(struct.public_send(method)).to eq(blueprint[1, 2, 3])
+        expect(struct.public_send(method)).to eq(struct.class[1, 2, 3])
       end
     end
 
     context "with keyword construction" do
-      let(:blueprint) { Struct.new :a, :b, :c, keyword_init: true }
-      let(:struct) { blueprint[a: 1, b: 2, c: 3] }
+      let(:struct) { Struct.new(:a, :b, :c, keyword_init: true).new a: 1, b: 2, c: 3 }
 
-      it "answers struct with all attributes rebuilt" do
-        expect(struct.public_send(method, a: 7, b: 8, c: 9)).to eq(blueprint[a: 7, b: 8, c: 9])
+      it "answers struct with attributes rebuilt" do
+        expect(struct.public_send(method, a: 7, b: 8, c: 9)).to eq(struct.class[a: 7, b: 8, c: 9])
       end
 
       it "answers struct with some attributes rebuilt" do
-        expect(struct.public_send(method, a: 7, c: 9)).to eq(blueprint[a: 7, b: 2, c: 9])
+        expect(struct.public_send(method, a: 7, c: 9)).to eq(struct.class[a: 7, b: 2, c: 9])
       end
 
       it "answers identical struct with no arguments" do
-        expect(struct.public_send(method)).to eq(blueprint[a: 1, b: 2, c: 3])
+        expect(struct.public_send(method)).to eq(struct.class[a: 1, b: 2, c: 3])
       end
     end
   end
@@ -113,19 +111,17 @@ RSpec.describe Refinements::Structs do
     it_behaves_like "a merge", :merge
 
     it "doesn't mutate itself with positional construction" do
-      blueprint = Struct.new :a, :b, :c
-      struct = blueprint[1, 2, 3]
+      struct = Struct.new(:a, :b, :c).new 1, 2, 3
       struct.merge a: 7, b: 8, c: 9
 
-      expect(struct).to eq(blueprint[1, 2, 3])
+      expect(struct).to eq(struct.class[1, 2, 3])
     end
 
     it "doesn't mutate itself with keyword construction" do
-      blueprint = Struct.new :a, :b, :c, keyword_init: true
-      struct = blueprint[a: 1, b: 2, c: 3]
+      struct = Struct.new(:a, :b, :c, keyword_init: true).new a: 1, b: 2, c: 3
       struct.merge a: 7, b: 8, c: 9
 
-      expect(struct).to eq(blueprint[a: 1, b: 2, c: 3])
+      expect(struct).to eq(struct.class[a: 1, b: 2, c: 3])
     end
   end
 
@@ -133,31 +129,29 @@ RSpec.describe Refinements::Structs do
     it_behaves_like "a merge", :merge!
 
     it "mutates itself with positional construction" do
-      blueprint = Struct.new :a, :b, :c
-      struct = blueprint[1, 2, 3]
+      struct = Struct.new(:a, :b, :c).new 1, 2, 3
       struct.merge! a: 7, b: 8, c: 9
 
-      expect(struct).to eq(blueprint[7, 8, 9])
+      expect(struct).to eq(struct.class[7, 8, 9])
     end
 
     it "mutates itself with keyword construction" do
-      blueprint = Struct.new :a, :b, :c, keyword_init: true
-      struct = blueprint[a: 1, b: 2, c: 3]
+      struct = Struct.new(:a, :b, :c, keyword_init: true).new a: 1, b: 2, c: 3
       struct.merge! a: 7, b: 8, c: 9
 
-      expect(struct).to eq(blueprint[a: 7, b: 8, c: 9])
+      expect(struct).to eq(struct.class[a: 7, b: 8, c: 9])
     end
   end
 
   shared_examples_for "a revalue" do |method|
     it "answers transformed values with block" do
       expectation = struct.public_send(method) { |value| value * 2 }
-      expect(expectation).to eq(blueprint.new.merge(a: 2, b: 4, c: 6))
+      expect(expectation).to eq(struct.class.new.merge(a: 2, b: 4, c: 6))
     end
 
     it "answers transformed values with hash and block" do
       expectation = struct.public_send(method, b: 1) { |previous, current| previous + current }
-      expect(expectation).to eq(blueprint.new.merge(a: 1, b: 3, c: 3))
+      expect(expectation).to eq(struct.class.new.merge(a: 1, b: 3, c: 3))
     end
 
     it "answers itself when block isn't given" do
@@ -167,52 +161,48 @@ RSpec.describe Refinements::Structs do
 
   describe "#revalue" do
     context "with positional construction" do
-      let(:blueprint) { Struct.new :a, :b, :c }
-      let(:struct) { blueprint[1, 2, 3] }
+      let(:struct) { Struct.new(:a, :b, :c).new 1, 2, 3 }
 
       it_behaves_like "a revalue", :revalue
 
       it "doesn't mutate itself" do
         struct.revalue { |value| value * 2 }
-        expect(struct).to eq(blueprint[1, 2, 3])
+        expect(struct).to eq(struct.class[1, 2, 3])
       end
     end
 
     context "with keyword construction" do
-      let(:blueprint) { Struct.new :a, :b, :c, keyword_init: true }
-      let(:struct) { blueprint[a: 1, b: 2, c: 3] }
+      let(:struct) { Struct.new(:a, :b, :c, keyword_init: true).new a: 1, b: 2, c: 3 }
 
       it_behaves_like "a revalue", :revalue
 
       it "doesn't mutate itself" do
         struct.revalue { |value| value * 2 }
-        expect(struct).to eq(blueprint[a: 1, b: 2, c: 3])
+        expect(struct).to eq(struct.class[a: 1, b: 2, c: 3])
       end
     end
   end
 
   describe "#revalue!" do
     context "with positional construction" do
-      let(:blueprint) { Struct.new :a, :b, :c }
-      let(:struct) { blueprint[1, 2, 3] }
+      let(:struct) { Struct.new(:a, :b, :c).new 1, 2, 3 }
 
       it_behaves_like "a revalue", :revalue!
 
       it "mutates itself" do
         struct.revalue! { |value| value * 2 }
-        expect(struct).to eq(blueprint[2, 4, 6])
+        expect(struct).to eq(struct.class[2, 4, 6])
       end
     end
 
     context "with keyword construction" do
-      let(:blueprint) { Struct.new :a, :b, :c, keyword_init: true }
-      let(:struct) { blueprint[a: 1, b: 2, c: 3] }
+      let(:struct) { Struct.new(:a, :b, :c, keyword_init: true).new a: 1, b: 2, c: 3 }
 
       it_behaves_like "a revalue", :revalue!
 
       it "mutates itself" do
         struct.revalue! { |value| value * 2 }
-        expect(struct).to eq(blueprint[a: 2, b: 4, c: 6])
+        expect(struct).to eq(struct.class[a: 2, b: 4, c: 6])
       end
     end
   end
