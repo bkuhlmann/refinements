2ed9f17cd314c8cd07125eb3b6f30eb6113041da
Brooke Kuhlmann <brooke@alchemists.io>
2023-04-03 07:39:40 -0600

    Fixed RuboCop Style/FormatStringToken issues

--- lib/refinements/big_decimals.rb
+++ lib/refinements/big_decimals.rb
@@ -6,7 +6,7 @@ module Refinements
   # Provides additional enhancements to the BigDecimal primitive.
   module BigDecimals
     refine BigDecimal do
-      def inspect = format("#<BigDecimal:%{id} %{string}>", id: object_id, string: to_s("F"))
+      def inspect = format("#<BigDecimal:%<id>s %<string>s>", id: object_id, string: to_s("F"))
     end
   end
 end
